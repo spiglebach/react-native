c30824b790ea5e3bcbac8a54ec78b69945a62c85
Szukács Marcell <spiglebach@gmail.com>
2025-01-27 18:51:55 +0100

    add transparent background image

--- target-app/App.js
+++ target-app/App.js
@@ -1,18 +1,26 @@
-import { StyleSheet } from 'react-native';
+import { ImageBackground, StyleSheet } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
     return (
         <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.root}>
-            <StartGameScreen />
+            <ImageBackground
+                    source={require('./assets/images/background.png')}
+                    resizeMode='cover'
+                    style={styles.root}
+                    imageStyle={styles.backgroundImage}>
+                <StartGameScreen />
+            </ImageBackground>
         </LinearGradient>
     );
 }
 
 const styles = StyleSheet.create({
     root: {
-        flex: 1,
-        backgroundColor: '#ddb52f'
+        flex: 1
+    },
+    backgroundImage: {
+        opacity: 0.15
     }
 });
