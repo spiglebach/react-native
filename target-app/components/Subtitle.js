70d563027269a3d16e3c8f35000078c91a58c494
Szukács Marcell <spiglebach@gmail.com>
2025-02-04 16:04:28 +0100

    guess buttons and guess history

---
+++ target-app/components/Subtitle.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text } from "react-native"
+import Colors from "../constants/colors"
+
+function Subtitle({children}) {
+    return (
+        <Text style={styles.subtitle}>{children}</Text>
+    )
+}
+
+const styles = StyleSheet.create({
+    subtitle: {
+        width: '80%',
+        marginHorizontal: 'auto',
+        fontSize: 18,
+        color: Colors.primary500,
+        textAlign: 'center',
+        borderBottomWidth: 1,
+        borderColor: Colors.primary500,
+        padding: 6,
+    }
+})
+
+export default Subtitle
--- target-app/screens/GameScreen.js
+++ target-app/screens/GameScreen.js
@@ -1,16 +1,69 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import Title from "../components/Title";
+import { useState } from "react";
+import Colors from "../constants/colors";
+import PrimaryButton from "../components/PrimaryButton";
+import Subtitle from "../components/Subtitle";
 
-function GameScreen() {
+function generateRandomNumberBetween(min, max, exclude) {
+    const randomNumber = Math.floor(Math.random() * (max - min)) + min
+
+    if (exclude && randomNumber === exclude) {
+        return generateRandomNumberBetween(min, max, exclude)
+    } else {
+        return randomNumber
+    }
+}
+
+let minimumGuess = 1
+let maximumGuess = 100
+
+function GameScreen({pickedNumber}) {
+    const [guess, setGuess] = useState(generateRandomNumberBetween(minimumGuess, maximumGuess, pickedNumber))
+    const [guessHistory, setGuessHistory] = useState([])
+
+    function nextGuessHandler(direction) { // direction: 'lower' or 'higher'
+        if ((direction === 'lower' && guess < pickedNumber) || (direction === 'higher' && guess > pickedNumber)) {
+            Alert.alert("Don't lie!", "You know that this is wrong...", [
+                {text: 'Sorry!', style: 'cancel'}
+            ])
+            return
+        }
+        if (direction === 'lower') {
+            maximumGuess = guess
+        } else if (direction === 'higher') {
+            minimumGuess = guess + 1
+        }
+        setGuessHistory(previousGuessHistory => [...previousGuessHistory, guess])
+        setGuess(generateRandomNumberBetween(minimumGuess, maximumGuess))
+            
+    }
+
+    if (guess === pickedNumber) {
+        console.log("Game over, match found!")
+        return
+    }
+
     return (
         <View style={styles.screen}>
             <Title>Opponent's Guess</Title>
-            <View>
-                <Text>Lower or Higher?</Text>
-                <Text>+ -</Text>
+            <View style={styles.guessContainer}>
+                <Text style={styles.guessText}>{guess}</Text>
             </View>
             <View>
-                <Text>Log rounds</Text>
+                <Subtitle>Lower or Higher?</Subtitle>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>Lower</PrimaryButton>
+                    <PrimaryButton onPress={nextGuessHandler.bind(this, 'higher')}>Higher</PrimaryButton>
+                </View>
+            </View>
+            <View style={styles.historyContainer}>
+                <Subtitle>Guess history</Subtitle>
+                {guessHistory.map((historicalGuess, index) => {
+                    return (
+                        <Text key={`guess-${index}`} style={styles.guessHistoryText}>Guess #{index + 1}: {historicalGuess}</Text>
+                    )
+                })}
             </View>
         </View>
     )
@@ -18,9 +71,33 @@ function GameScreen() {
 
 const styles = StyleSheet.create({
     screen: {
-        flex: 1,
         marginTop: 50,
-        padding: 30
+        marginHorizontal: 30,
+        borderRadius: 20,
+        padding: 30,
+        backgroundColor: Colors.accent
+    },
+    guessContainer: {
+        alignItems: 'center'
+    },
+    guessText: {
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: 'bold',
+        padding: 12
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: 10,
+        marginTop: 10
+    },
+    historyContainer: {
+        marginTop: 20
+    },
+    guessHistoryText: {
+        padding: 6,
+        textAlign: 'center'
     }
 })
 
