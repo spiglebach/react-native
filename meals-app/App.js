8d50ebfe09974ae1f9f11532c7acbd774d64553e
Szukács Marcell <spiglebach@gmail.com>
2025-02-13 16:57:15 +0100

    add statusbar

--- meals-app/App.js
+++ meals-app/App.js
@@ -1,10 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
 import CategoriesScreen from './screens/CategoriesScreen';
 import * as SystemUI from "expo-system-ui"
 
 export default function App() {
     SystemUI.setBackgroundColorAsync("#aa2288")
     return (
+        <>
+        <StatusBar style='light'/>
         <CategoriesScreen />
+        </>
     )
 }
 
