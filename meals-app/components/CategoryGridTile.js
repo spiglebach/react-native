16e2f2871264169cad2b64068da16524976b5dd0
Szukács Marcell <spiglebach@gmail.com>
2025-02-13 16:48:12 +0100

    button press styling

--- meals-app/components/CategoryGridTile.js
+++ meals-app/components/CategoryGridTile.js
@@ -1,10 +1,12 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Platform, Pressable, StyleSheet, Text, View } from "react-native";
 
 function CategoryGridTile({title, color}) {
     return (
-        <View style={[styles.gridItem, {backgroundColor: color}]}>
-            <Pressable style={styles.button}>
-                <View style={styles.innerContainer}>
+        <View style={[styles.gridItem]}>
+            <Pressable
+                    android_ripple={{color: '#ccc'}}
+                    style={({pressed}) => [styles.button, pressed ? styles.buttonPressed : null]}>
+                <View style={[styles.innerContainer, {backgroundColor: color}]}>
                     <Text>{title}</Text>
                 </View>
             </Pressable>
@@ -22,14 +24,19 @@ const styles = StyleSheet.create({
         shadowColor: 'black',
         shadowOffset: {width: 0, height: 2},
         shadowOpacity: 0.25,
-        shadowRadius: 8
+        shadowRadius: 8,
+        overflow: Platform.OS === 'android' ? 'hidden' : 'visible'
     },
     button: {
         flex: 1
     },
+    buttonPressed: {
+        opacity: 0.5
+    },
     innerContainer: {
         flex: 1,
         padding: 16,
+        borderRadius: 8,
         justifyContent: 'center',
         alignItems: 'center'
     },
