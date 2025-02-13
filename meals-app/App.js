690d91321af3128c4d2309f897b1176c94d039a9
Szukács Marcell <spiglebach@gmail.com>
2025-02-13 17:05:28 +0100

    stack navigation

--- meals-app/App.js
+++ meals-app/App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import CategoriesScreen from './screens/CategoriesScreen';
 import * as SystemUI from "expo-system-ui"
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
     SystemUI.setBackgroundColorAsync("#aa2288")
     return (
         <>
-        <StatusBar style='light'/>
-        <CategoriesScreen />
+        <StatusBar style='dark'/>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="MealCategories" component={CategoriesScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
         </>
     )
 }
