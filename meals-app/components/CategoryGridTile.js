0d48769469cd38694f1c1fa32ece63785273208c
Szukács Marcell <spiglebach@gmail.com>
2025-02-13 17:13:57 +0100

    meals overview screen and navigating between screens via the navigation prop

--- meals-app/components/CategoryGridTile.js
+++ meals-app/components/CategoryGridTile.js
@@ -1,9 +1,10 @@
 import { Platform, Pressable, StyleSheet, Text, View } from "react-native";
 
-function CategoryGridTile({title, color}) {
+function CategoryGridTile({title, color, onPress}) {
     return (
         <View style={[styles.gridItem]}>
             <Pressable
+                    onPress={onPress}
                     android_ripple={{color: '#ccc'}}
                     style={({pressed}) => [styles.button, pressed ? styles.buttonPressed : null]}>
                 <View style={[styles.innerContainer, {backgroundColor: color}]}>
