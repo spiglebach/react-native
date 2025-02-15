d3905f1cc0987ef20d41fb0065e8d23d94dd9e9e
Szukács Marcell <spiglebach@gmail.com>
2025-02-15 13:34:19 +0100

    useNavigation hook

--- meals-app/screens/CategoriesScreen.js
+++ meals-app/screens/CategoriesScreen.js
@@ -1,28 +1,29 @@
 import { FlatList } from 'react-native'
 import { CATEGORIES } from '../data/dummy-data'
 import CategoryGridTile from '../components/CategoryGridTile'
+import { useNavigation } from '@react-navigation/native'
 
-function CategoriesScreen({navigation}) {
-
-    function renderCategoryItem(itemData) {
-        function pressHandler() {
-            navigation.navigate("MealsOverview")
-        }
-    
-        return (
-            <CategoryGridTile
-                title={itemData.item.title}
-                color={itemData.item.color}
-                onPress={pressHandler}    
-            />
-        )
+function CategoryItem({itemData}) {
+    const navigation = useNavigation()
+    function pressHandler() {
+        navigation.navigate("MealsOverview")
     }
 
+    return (
+        <CategoryGridTile
+            title={itemData.item.title}
+            color={itemData.item.color}
+            onPress={pressHandler}    
+        />
+    )
+}
+
+function CategoriesScreen({navigation}) {
     return (
         <FlatList
             data={CATEGORIES}
             keyExtractor={item => item.id}
-            renderItem={renderCategoryItem}
+            renderItem={itemData => <CategoryItem itemData={itemData} />}
             numColumns={2}
         />
     )
