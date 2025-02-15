b7aa42df0a70fbcca5e152f51fcc9ac296a4dfc7
Szukács Marcell <spiglebach@gmail.com>
2025-02-15 15:32:22 +0100

    meal details screen content and styling #1

--- meals-app/screens/MealDetailsScreen.js
+++ meals-app/screens/MealDetailsScreen.js
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native"
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native"
 import { MEALS } from "../data/dummy-data"
 import { useLayoutEffect } from "react"
 
@@ -12,11 +12,88 @@ function MealDetailsScreen({route, navigation}) {
         })
     }, [navigation, mealId])
 
+    const {
+        title,
+        affordability,
+        complexity,
+        imageUrl,
+        duration,
+        ingredients,
+        steps,
+        isGlutenFree,
+        isVegan,
+        isVegetarian,
+        isLactoseFree
+    } = meal
+
     return (
-        <View>
-            <Text>{meal.title}</Text>
-        </View>
+        <ScrollView style={styles.outerContainer}>
+            <Image style={styles.image} source={{
+                uri: imageUrl
+            }} />
+            <Text style={styles.title}>{title}</Text>
+            <View style={[styles.section, styles.quickDetails]}>
+                <Text>{duration}m</Text>
+                <Text>{affordability}</Text>
+                <Text>{complexity}</Text>
+            </View>
+            <View style={styles.section}>
+                <Text style={styles.sectionTitle}>Ingredients:</Text>
+                <View style={styles.sectionContent}>
+                    {ingredients.map(ingredient => <Text>{ingredient}</Text>)}
+                </View>
+            </View>
+            <View style={styles.section}>
+                <Text style={styles.sectionTitle}>Steps:</Text>
+                <View style={styles.sectionContent}>
+                    {steps.map((step, index) => <Text>{index + 1}: {step}</Text>)}
+                </View>
+            </View>
+            <View style={styles.section}>
+                <Text style={styles.sectionTitle}>Accessibility:</Text>
+                <View style={styles.sectionContent}>
+                    <Text>{isGlutenFree ? 'gluten free' : null}</Text>
+                    <Text>{isVegan ? 'vegan' : null}</Text>
+                    <Text>{isVegetarian ? 'vegetarian' : null}</Text>
+                    <Text>{isLactoseFree ? 'lactose free' : null}</Text>
+                </View>
+            </View>
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    outerContainer: {
+        flex: 1,
+        backgroundColor: 'wheat'
+    },
+    image: {
+        width: '100%',
+        height: 300,
+        objectFit: 'fill'
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: 'bold',
+        paddingVertical: 14
+    },
+    quickDetails: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: 14
+    },
+    section: {
+        marginTop: 14,
+        paddingHorizontal: 14
+    },
+    sectionTitle: {
+        textAlign: 'center',
+        fontSize: 16
+    },
+    sectionContent: {
+        gap: 4
+    }
+})
 
 export default MealDetailsScreen
