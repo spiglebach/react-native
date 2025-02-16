3b44e2e13c73d2532f22b9f0c440c4e1f629d122
Szukács Marcell <spiglebach@gmail.com>
2025-02-16 15:15:31 +0100

    add no favourites text

--- meals-app/screens/FavouritesScreen.js
+++ meals-app/screens/FavouritesScreen.js
@@ -2,13 +2,34 @@ import MealsList from "../components/MealsList";
 import { MEALS } from "../data/dummy-data";
 import { useContext } from "react";
 import { FavouritesContext } from "../store/context/favourites-context";
+import { StyleSheet, Text, View } from "react-native";
 
 function FavouritesScreen() {
     const favouriteMealsContext = useContext(FavouritesContext)
     const meals = MEALS.filter(meal => favouriteMealsContext.ids.includes(meal.id))
+
+    if (favouriteMealsContext.ids.length <= 0) {
+        return (
+            <View style={styles.rootContainer}>
+                <Text style={styles.text}>You have no favourite meals yet.</Text>
+            </View>
+        )
+    }
     return (
         <MealsList meals={meals} />
     )
 }
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: 20,
+        color: 'silver'
+    }
+})
 
 export default FavouritesScreen
