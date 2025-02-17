4794fbd02da35a338102ac2de4f6ee2c8f5c1cc7
Szukács Marcell <spiglebach@gmail.com>
2025-02-17 18:00:52 +0100

    navigation components styling

--- expense-tracker/App.js
+++ expense-tracker/App.js
@@ -6,6 +6,8 @@ import AllExpensesScreen from './screens/AllExpenses';
 import { NavigationContainer } from '@react-navigation/native'
 import ManageExpenseScreen from './screens/ManageExpense';
 import { Button } from 'react-native';
+import { GlobalStyles } from './constants/styles';
+import { Ionicons } from '@expo/vector-icons'
 
 const BottomTabs = createBottomTabNavigator()
 const Stack = createNativeStackNavigator()
@@ -14,15 +16,38 @@ function ViewExpensesNavigator({navigation}) {
     function handleNavigation() {
         navigation.navigate('ManageExpense')
     }
-    const navButton = <Button title='+' onPress={handleNavigation} />
+    const navButton = <Button color={GlobalStyles.colors.headerTintColor} title='+' onPress={handleNavigation} />
 
     return (
         <BottomTabs.Navigator
             screenOptions={{
-                headerRight: () => navButton
+                headerRight: () => navButton,
+                headerStyle: {
+                    backgroundColor: GlobalStyles.colors.headerBackground
+                },
+                tabBarStyle: {
+                    backgroundColor: GlobalStyles.colors.headerBackground
+                },
+                tabBarActiveTintColor: GlobalStyles.colors.activeTabColor,
+                tabBarInactiveTintColor: GlobalStyles.colors.inactiveTabColor,
+                headerTintColor: GlobalStyles.colors.headerTintColor
             }}>
-            <BottomTabs.Screen name='RecentExpenses' component={RecentExpensesScreen} />
-            <BottomTabs.Screen name='AllExpenses' component={AllExpensesScreen} />
+            <BottomTabs.Screen
+                name='RecentExpenses'
+                component={RecentExpensesScreen}
+                options={{
+                    title: 'Recent Expenses',
+                    tabBarIcon: ({color, size}) => <Ionicons name='hourglass' size={size} color={color} />
+                }}
+            />
+            <BottomTabs.Screen
+                name='AllExpenses'
+                component={AllExpensesScreen}
+                options={{
+                    title: 'All Expenses',
+                    tabBarIcon: ({color, size}) => <Ionicons name='calendar' size={size} color={color} />
+                }}
+            />
         </BottomTabs.Navigator>
     )
 }
@@ -30,9 +55,14 @@ function ViewExpensesNavigator({navigation}) {
 export default function App() {
     return (
         <>
-        <StatusBar style="auto" />
+        <StatusBar style="light" />
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator screenOptions={{
+                headerStyle: {
+                    backgroundColor: GlobalStyles.colors.headerBackground
+                },
+                headerTintColor: GlobalStyles.colors.headerTintColor
+            }}>
                 <Stack.Screen
                     name='ViewExpenses'
                     component={ViewExpensesNavigator}
@@ -40,7 +70,13 @@ export default function App() {
                         headerShown: false
                     }}
                 />
-                <Stack.Screen name='ManageExpense' component={ManageExpenseScreen} />
+                <Stack.Screen
+                    name='ManageExpense'
+                    component={ManageExpenseScreen} 
+                    options={{
+                        title: 'Manage Expenses'
+                    }}
+                />
             </Stack.Navigator>
         </NavigationContainer>
         </>
---
+++ expense-tracker/constants/styles.js
@@ -0,0 +1,8 @@
+export const GlobalStyles = {
+    colors: {
+        headerBackground: 'mediumpurple',
+        headerTintColor: 'mistyrose',
+        activeTabColor: 'gold',
+        inactiveTabColor: 'silver',
+    }
+}
