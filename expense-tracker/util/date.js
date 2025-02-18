cc0e9ab112e737fcc49de7ce78ef29fb5fe80206
Szukács Marcell <spiglebach@gmail.com>
2025-02-18 18:51:34 +0100

    date format fix

--- expense-tracker/util/date.js
+++ expense-tracker/util/date.js
@@ -3,7 +3,11 @@ export function getFormattedDate(date) {
     if (month < 10) {
         month = `0${month}`
     }
-    return `${date.getFullYear()}-${month}-${date.getDate()}`
+    let day = date.getDate()
+    if (day < 10) {
+        day = `0${day}`
+    }
+    return `${date.getFullYear()}-${month}-${day}`
 }
 
 export function getDateMinusDays(date, days) {
